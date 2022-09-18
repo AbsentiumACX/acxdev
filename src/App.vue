40936918cc85d770e44ae2742f354b3f7b015e2d
Jeroen Kraaijkamp <jkraaijkamp@gmail.com>
2022-09-19 00:15:45 +0200

    added the content retrieval

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,41 @@
 <template>
   <div id="app">
-    <HeaderComponent />
+    <div class="row">
+      <HeaderComponent />
+    </div>
+    <ContentComponent :content="content" />
   </div>
 </template>
 
 <script>
-import HeaderComponent from './components/HeaderComponent.vue';
+  import axios from 'axios';
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import ContentComponent from './components/ContentComponent.vue';
+  import { defineCustomElement } from 'vue'
+  import TableComponent from '@/components/ContentComponents/TableComponent.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HeaderComponent,
-}
-}
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent,
+      ContentComponent
+    }, 
+    data() {
+      return {
+        content: ['title', 'content']
+      }
+    },
+    created: function() {
+      axios.get('http://localhost:8000/api/content' + window.location.pathname)
+        .then(res=> {
+          this.content = res.data
+      }).then(function() {        
+        const Graph = defineCustomElement(TableComponent)
+
+        customElements.define('skill-graph', Graph)
+      })
+    }
+  }
 </script>
 
 <style>
@@ -21,6 +44,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 </style>
---
+++ src/components/ContentComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <div class="jumbotron bg-dark col-md-12">
+            <h1>{{content.title}}</h1>
+        </div>
+        <div class="col-md-3"></div>
+        <div class="col-md-6 text-left" v-html="content.content"></div>
+        <div class="col-md-3"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ContentComponent',
+        props: ['content'],
+    }
+</script>
+
+<style>
+    .jumbotron {
+        background-image: url('~@/assets/jumbotron.jpg');
+        background-size: cover;
+        box-shadow:inset 0 0 0 200px rgba(0, 0, 0, 0.75);
+    }
+</style>
